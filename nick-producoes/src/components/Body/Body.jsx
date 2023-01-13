0443746d767cef257647e89c70ceb4d28ed5586d
Juliano Amaral <jaygovinda13@gmail.com>
2023-01-13 19:33:36 -0300

    emplementando Body, melhoramentos do Header

--- nick-producoes/src/components/Body/Body.jsx
+++ nick-producoes/src/components/Body/Body.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import React from "react";
 
-const Body = () => {
+
+function BackgroundImagePage() {
   return (
-    <div>Body</div>
-  )
+    <Box
+      style={{
+        backgroundColor: "black",
+        height:"720px",
+        width:"1440px",
+        position:"static",
+      }}
+    >
+        <Box component="img" src="/assets/fundo.png" height="20em" position="relative"/>
+        <Typography sx={{ color:"white"}} position="relative">
+        A Nick Te Poem no Mapa
+        </Typography>
+    </Box>
+  );
 }
 
-export default Body
+export default BackgroundImagePage;
